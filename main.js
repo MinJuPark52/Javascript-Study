528e85d1bd295bcba72e22a4032a3ee615e42363
MinJuPark52 <als4013@naver.com>
2025-07-03 17:06:17 +0900

    use strict

---
+++ main.js
@@ -0,0 +1,5 @@
+// 자바스크립트 매우 유연 === 매우 위험
+// added ECMAScript 5 'use strict' 모드
+"use strict";
+
+console.log("hello world");
---
+++ variable.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// 2. 변수
+// let
+let name = "elie";
+console.log(name);
+name = "hello";
+console.log(name);
