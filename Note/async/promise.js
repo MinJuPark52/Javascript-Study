0b009578f43671618cc5f54c7f1bde28bbb46d1e
MinJuPark52 <als4013@naver.com>
2025-07-18 16:40:26 +0900

    [08]: promise(producer vs consumers)

--- Note/async/promise.js
+++ Note/async/promise.js
@@ -4,5 +4,62 @@
 // state(상태) : pending -> fulfilled or rejected
 // producer 와 consumer
 
-// 1. producer
-const promise = new Promise((resolve, reject) => {});
+// 1. Producer
+// 새로운 프로미스가 만들어질 때 excutor 함수가 자동으로 바로 실행됨
+const promise = new Promise((resolve, reject) => {
+  // (network, read files...) 비동기로 처리
+  console.log("doing something");
+  setTimeout(() => {
+    // resolve("ellie");  성공적으로 호출
+    reject(new Error("no network"));
+  }, 2000);
+});
+
+// 2. Consumers: then, catch, finally
+promise
+  .then((value) => {
+    // 성공한 값
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    // 무조건 수행되는 값
+    console.log("finally");
+  });
+
+// 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+  .then((num) => num * 2)
+  .then((num) => num * 3)
+  .then((num) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(num - 1), 1000);
+    });
+  })
+  .then((num) => console.log(num));
+
+// 4. Error Handling
+const getHen = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve("🐓"));
+  });
+const getEgg = (hen) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error(`error! ${hen} => 🥚`)), 1000);
+  });
+const cook = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve("🍳"));
+  });
+
+getHen() //
+  .then(getEgg)
+  .then(cook)
+  .then(console.log) // 🐓 => 🥚 => 🍳
+  .catch(console.log);
