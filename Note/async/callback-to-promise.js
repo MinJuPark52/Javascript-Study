62e695b6906bf46b5177045ef9f03f14bf011632
MinJuPark52 <als4013@naver.com>
2025-07-18 16:51:32 +0900

    [09]: promise 콜백지옥

---
+++ Note/async/callback-to-promise.js
@@ -0,0 +1,39 @@
+/* 콜백 지옥 */
+class USerStorage {
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "ellie" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 2000);
+    });
+  }
+
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "ellie") {
+          resolve({ name: "ellie", role: "admin" });
+        } else {
+          reject(new Error("no access"));
+        }
+      }, 1000);
+    });
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+userStorage.log;
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then((user) => alert(`hello ${user.name}, your have a ${user.role} role`))
+  .catch(console.log);
